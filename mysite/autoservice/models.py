c0f8eafa8e19298b4134079ad94a41bb22c4fe79
Agne Abromaite <agne.abromaite@gmail.com>
2023-04-26 12:23:11 +0300

    moelių pavadinimai atvaizduojami teisingai (vienaskaita ir daugiskaita)

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -8,12 +8,20 @@ class VehicleModel(models.Model):
     def __str__(self):
         return f"{self.make} {self.model}"
 
+    class Meta:
+        verbose_name = "Automobilio modelis"
+        verbose_name_plural = "Automobilio modeliai"
+
 class Service(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=50)
     price = models.IntegerField(verbose_name="Kaina")
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Paslauga"
+        verbose_name_plural = "Paslaugos"
 
 class Vehicle(models.Model):
     plate = models.CharField(verbose_name="Valstybinis numeris", max_length=6)
@@ -23,12 +31,21 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return f"{self.vehicle_model} ({self.plate}"
+
+    class Meta:
+        verbose_name = "Automobilis"
+        verbose_name_plural = "Automobiliai"
 class Order(models.Model):
     date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     vehicle = models.ForeignKey(to="Vehicle", verbose_name="Automobilis", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.vehicle} ({self.date}) "
+
+    class Meta:
+        verbose_name = "Užsakymas"
+        verbose_name_plural = "Užsakymai"
+
 class OrderLine(models.Model):
     order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
     service = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True)
@@ -36,3 +53,7 @@ class OrderLine(models.Model):
 
     def __str__(self):
         return f"{self.order.vehicle} ({self.order.date}): {self.service} - {self.quantity}"
+
+    class Meta:
+        verbose_name = "Užsakymo eilutė"
+        verbose_name_plural = "Užsakymo eilutės"
