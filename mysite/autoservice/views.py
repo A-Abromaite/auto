f03501d301d9af9f914bc2fa7a38019e58952deb
Agne Abromaite <agne.abromaite@gmail.com>
2023-04-27 13:02:10 +0300

    Padarytas RedirectView i autoservice, pridėta "atlikta užsakymų"

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -5,14 +5,14 @@ from .models import Service, VehicleModel, Vehicle, Order, OrderLine
 # Create your views here.
 def index(request):
 
-    num_services = Service.objects.all().count
-    num_vehicles = Vehicle.objects.all().count
-    # num_services_completed
+    num_services = Service.objects.all().count()
+    num_vehicles = Vehicle.objects.all().count()
+    num_services_completed = Order.objects.filter(status__exact="i").count()
 
     context = {
         'num_services': num_services,
         'num_vehicles': num_vehicles,
-        # 'num_services_completed': num_services_completed
+        'num_services_completed': num_services_completed
     }
 
 
